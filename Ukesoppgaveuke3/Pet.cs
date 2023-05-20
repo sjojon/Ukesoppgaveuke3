f7294a85e720a23969bbfe25be8a94369748bec3
sjojon <90796535+sjojon@users.noreply.github.com>
2023-05-20 16:12:20 +0200

    Initial commit

---
+++ Ukesoppgaveuke3/Pet.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ukesoppgaveuke3
+{
+    internal class Pet
+    {
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+        public string Breed { get; private set; }
+        public string FavoriteFood { get; private set; }
+
+
+        public Pet(string newName, int newAge, string newBreed, string newFavoriteFood)
+        {
+            Name = newName;
+            Age = newAge;
+            Breed = newBreed;
+            FavoriteFood = newFavoriteFood;
+        }
+
+
+        public void Feed()
+        {
+            Console.Clear();
+            Console.WriteLine($"Takk for maten!");
+            Console.WriteLine();
+        }
+
+        public void Feed(string name, string food)
+        {
+            if (name == Name && food == FavoriteFood)
+            {
+                Console.Clear();
+                Console.WriteLine($"Dette er det beste jeg vet!!");
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Ukesoppgaveuke3/Program.cs
@@ -0,0 +1,123 @@
+﻿using System.ComponentModel.Design;
+using System.Runtime.CompilerServices;
+using System.Xml.Linq;
+using Ukesoppgaveuke3;
+using static System.Reflection.Metadata.BlobBuilder;
+
+namespace Ukesoppgaveuke3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Pet> pets = new()
+            {
+                new Pet("Samba", 10, "Cat", "Fish"),
+                new Pet("Merida", 3, "Cat", "Salad"),
+                new Pet("Miko", 8, "Dog", "Sausage")
+            };
+            //pets.Add(new Pet("King", 10, "Parrot", "Bird seed"));
+            Menu(pets);
+        }
+
+        public static void Menu(List<Pet> pets)
+        {
+            Console.WriteLine("Virtual pet:");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Menu:\n1. Add pet\n2. Display pets\n3. Feed pets\n4. Exit");
+                string inputAnswer = Console.ReadLine();
+                bool number = int.TryParse(inputAnswer, out int answer);
+
+                if (number)
+                {
+                    switch (answer)
+                    {
+                        case 1:
+                            AddPet(pets);
+                            break;
+                        case 2:
+                            Console.Clear();
+                            ListPets(pets);
+                            break;
+                        case 3:
+                            FeedPet(pets);
+                            break;
+                        case 4:
+                            flag = false;
+                            break;
+                        default:
+                            Console.WriteLine("Please enter 1, 2, 3 or 4!");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a number!");
+                }
+            }
+        }
+
+        public static void AddPet(List<Pet> pets)
+        {
+            Console.WriteLine(
+                "Please enter a pet to add:");
+            Console.WriteLine("Name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Age:");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Breed:");
+            string breed = Console.ReadLine();
+            Console.WriteLine("Favorite food:");
+            string favoriteFood = Console.ReadLine();
+
+            pets.Add(new Pet(name, age, breed, favoriteFood));
+            Console.Clear();
+            ListPets(pets);
+        }
+
+        public static void FeedPet(List<Pet> pets)
+        {
+            Console.Clear();
+            ListPets(pets);
+            Console.WriteLine("What pet do you want to feed?");
+            string nameToFeed = Console.ReadLine();
+            Console.WriteLine("What do you want to feed it with?");
+            string food = Console.ReadLine();
+            //Pet.Feed(nameToFeed);
+            //Pet.Feed(nameToFeed, food);
+            Pet pet = pets.Find(x => x.Name == nameToFeed);
+            if (pet != null)
+            {
+                if (food == pet.FavoriteFood)
+                {
+                    pet.Feed(nameToFeed, food);
+                }
+                else
+                {
+                    pet.Feed();
+                }
+            }
+            else
+            {
+                Console.WriteLine("You don't own this pet!");
+            }
+        }
+
+        public static void ListPets(List<Pet> pets)
+        {
+            foreach (var pet in pets)
+            {
+                PetDetails(pet);
+            }
+        }
+
+        public static void PetDetails(Pet pet)
+        {
+            Console.WriteLine(
+                $"Name: {pet.Name}\nAge: {pet.Age}\nBreed: {pet.Breed}\nFavorite food: {pet.FavoriteFood}");
+            Console.WriteLine();
+        }
+    }
+}
